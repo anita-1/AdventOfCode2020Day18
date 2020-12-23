03fe40dde2d66aa231d0aef0ced666a1cd43696f
Anita Luo <luo_anita@yahoo.com>
2020-12-23 02:39:57 -0600

    Initial Commit

---
+++ README.txt
@@ -0,0 +1,54 @@
+File: Day18MathMain
+Author: Anita Luo
+Date: 12/23/2020
+
+Description:
+Find the sum of each line of input using specific operation priorities.
+Left to right, parenthesis, addition, then multiplication
+
+Assumptions:
+Each line consists of integers, *, (, )
+
+Example Input:
+1 + (2 * 3) + (4 * (5 + 6))
+2 * 3 + (4 * 5)
+5 + (8 * 3 + 9 + 3 * 4 * 3)
+5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))
+((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2
+
+Example Output:
+"C:\Program Files\Java\jdk-12.0.2\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2019.3.3\lib\idea_rt.jar=52487:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2019.3.3\bin" -Dfile.encoding=UTF-8 -classpath "C:\Users\unebe\Documents\2020 F\AdventOfCode\out\production\AdventOfCode" Day18MathMain
+[1, +, (, 2, *, 3, ), +, (, 4, *, (, 5, +, 6, ), )]
+result: 51
+totalsum1: 51
+[2, *, 3, +, (, 4, *, 5, )]
+result: 46
+totalsum2: 97
+[5, +, (, 8, *, 3, +, 9, +, 3, *, 4, *, 3, )]
+result: 1445
+totalsum3: 1542
+[5, *, 9, *, (, 7, *, 3, *, 3, +, 9, *, 3, +, (, 8, +, 6, *, 4, ), )]
+result: 669060
+totalsum4: 670602
+[(, (, 2, +, 4, *, 9, ), *, (, 6, +, 9, *, 8, +, 6, ), +, 6, ), +, 2, +, 4, *, 2]
+result: 23340
+totalsum5: 693942
+
+Answer: 693942
+
+Approach:
+Read file
+Read each line
+Extract inside the insides of parenthesis from left to right in findSum() and send to mathString()
+Find addition sum in mathString()
+    do additions
+        remove the used elements
+        add the result back into list
+    if there are other elements after the additions, send to mulString() otherwise send result back to findSum()
+Find product result in mulString()
+    send result back to mathString()
+remove used elements
+add result sum back into list
+next parenthesis
+return line sum to main()
+Add line sum to product
---
+++ src/Day18MathMain.java
@@ -0,0 +1,182 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Day18MathMain
+{
+    public static void main(String[] args) throws Exception
+    {
+        //get file
+        File file = new File("C:\\Users\\unebe\\Documents\\2020 F\\AdventOfCode\\test.txt");
+
+        //read file
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        //add each line to lines
+        String line;
+        ArrayList<String> lines = new ArrayList<String>();
+        while ((line = br.readLine()) != null)
+        {
+            lines.add(line);
+        }
+
+        //amount of lines
+        int numLines = lines.size();
+
+        //info
+        String space = new String(" ");
+        ArrayList<String> chars = new ArrayList<String>();
+        long sum = 0;
+
+        //for each line find result and then total sum
+        for(int i = 0; i < numLines; i++)
+        {
+            line = lines.get(i);
+            for(int j = 0; j < line.length(); j++)
+            {
+                if(!space.equals("" + line.charAt(j)))
+                {
+                    chars.add("" + line.charAt(j));
+                }
+            }
+
+            //prints
+            System.out.println(chars);
+            //System.out.println("before sum: " + sum);
+            long result = findSum(chars);
+            System.out.println("result: " + result);
+            sum = sum + result;
+            System.out.println("totalsum" + (i + 1) + ": " + sum);
+            //System.out.println();
+
+            //clear chars
+            chars.clear();
+        }
+    }
+
+    //find the sum of a chars list, chars list unedited, as parenthesis
+    static long findSum(ArrayList<String> charsList)
+    {
+        //info
+        long sum = 0;
+        int indexOpen = 0;
+        int indexClose = 0;
+        int numBraces = Collections.frequency(charsList, "(");
+
+        //do math on inside of pairs of parenthesis and get rid of them
+        for(numBraces = numBraces; numBraces > 0; numBraces--)
+        {
+            //create a new arraylist to store chars in parenthesis
+            ArrayList<String> newChars = new ArrayList<String>();
+
+            //find first index of )
+            indexClose = charsList.indexOf(")");
+
+            //add elements from 0 to )
+            for(int index = 0; index < indexClose; index++)
+            {
+                newChars.add(charsList.get(index));
+            }
+
+            //find the last index of ( inside new list of chars
+            indexOpen = newChars.lastIndexOf("(");
+
+            //remove beginning to ( of list of chars
+            for(int index = 0; index < indexOpen; index++)
+            {
+                newChars.remove(0);
+            }
+
+            //remove ( inside of new list of chars
+            newChars.remove(0);
+
+            //send new list of chars to mathString to get result
+            long resultIt = mathString(newChars);
+
+            //make long to String
+            String resultItS = "" + resultIt;
+
+            //remove used elements from charsList
+            for(int times = 0; times < (indexClose - indexOpen + 1); times++)
+            {
+                charsList.remove(indexOpen);
+            }
+
+            //put sum back into charsList
+            charsList.add(indexOpen, resultItS);
+        }
+
+        //parenthesis are done, if still has math, send to mathString, otherwise get index 0
+        if(charsList.size() > 1)
+        {
+            sum = mathString(charsList);
+        }else
+        {
+            sum = Integer.parseInt(charsList.get(0));
+        }
+
+        //return sum
+        return sum;
+    }
+
+    //find the sum of a chars list, chars list is only math
+    static long mathString(ArrayList<String> charsList)
+    {
+        //info
+        //long mathResult = Long.parseLong(charsList.get(0));
+        long mathResult = 0;
+        int numAdds = Collections.frequency(charsList, "+");
+
+        //do addition on the elements and get rid of them
+        for(numAdds = numAdds; numAdds > 0 ; numAdds--)
+        {
+            //info
+            int index = charsList.indexOf("+");
+            long valPrev = Long.parseLong(charsList.get(index - 1));
+            long valNext = Long.parseLong(charsList.get(index + 1));
+            long resultHere = valPrev + valNext;
+            String  resultHereS = "" + resultHere;
+
+            //remove
+            charsList.remove(index - 1);
+            charsList.remove(index - 1);
+            charsList.remove(index - 1);
+
+            //add
+            charsList.add((index - 1), resultHereS);
+
+        }
+
+        //if theres anything left, do multiplications, otherwise return first element
+        if(charsList.size() > 1)
+        {
+            mathResult = mulString(charsList);
+        }
+        else
+        {
+            mathResult = Long.parseLong(charsList.get(0));
+        }
+
+        //return math result
+        return mathResult;
+    }
+
+    //find the sum of a chars list, chars list is only multiply
+    static long mulString(ArrayList<String> charsList)
+    {
+        //info
+        //start w first element of result
+        long mulResult = Long.parseLong(charsList.get(0));
+
+        //iterate through list and multiply
+        for(int index = 1; index < charsList.size(); index++)
+        {
+            mulResult = mulResult * Long.parseLong(charsList.get(index + 1));
+            index++;
+        }
+
+        //return result
+        return mulResult;
+    }
+}
+
