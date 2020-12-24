c0d451351c0c9c019e6a4f400846ccfc65875ee7
Anita Luo <luo_anita@yahoo.com>
2020-12-24 05:05:53 -0600

    complete day2

--- README.txt
+++ README.txt
@@ -12,7 +12,8 @@ Assumptions:
 each input line is an integer
 
 Technical use:
-ArrayLists, Integers
+Java
+read File, ArrayList, Integers
 
 Approach:
 nested for loops
@@ -32,6 +33,36 @@ Example Output:
 241861950
 
 * * *
+File: Day2Main.java
+Date: 12/24/2020
+
+Description:
+parse lines, find number of lines fulfill rule
+rule -xor chars at 2 places
+
+Assumptions:
+format of each line -<integer>-<integer> <charKey>: <password>
+
+Technical use:
+Java
+read File, ArrayLists, ints
+
+Approach:
+parse each line as String, get info
+do comparisions
+    add to variable
+print variable
+
+
+Example Input:
+1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc
+
+Example Output:
+1
+
+* * *
 File: Day18MathMain.java
 Date: 12/23/2020
 
@@ -44,7 +75,7 @@ Each line consists of integers, *, (, )
 
 Technical use:
 Java
-data types: long, ArrayList
+read File, long, ArrayList
 
 Approach:
 Read file
--- src/Day2Main.java
+++ src/Day2Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Day2Main
 {
@@ -13,12 +14,42 @@ public class Day2Main
         //read file
         BufferedReader br = new BufferedReader(new FileReader(file));
 
+        //initialize number of valid passwords variable
+        int validPswds = 0;
+
         //add each line to lines
         String line;
-        ArrayList<Integer> nums = new ArrayList<Integer>();
         while ((line = br.readLine()) != null)
         {
-            nums.add(Integer.parseInt(line));
+            //info
+            int dashIndex = line.indexOf("-");
+            int firstOccur = Integer.parseInt(String.valueOf(line.substring(0, dashIndex)));
+            int firstSpaceIndex = line.indexOf(" ");
+            int secondOccur = Integer.parseInt(String.valueOf(line.substring((dashIndex + 1), firstSpaceIndex)));
+            String key = "" + line.charAt((firstSpaceIndex + 1));
+            String pswd = line.substring((line.lastIndexOf(" ") + 1));
+            ArrayList<String> pswdChars = new ArrayList<String>();
+            for(int index = 0; index < pswd.length(); index++)
+            {
+                pswdChars.add("" + pswd.charAt(index));
+            }
+            int freqKey = Collections.frequency(pswdChars, key);
+            String charFirst = "" + pswd.charAt((firstOccur - 1));
+            String charSecond = "" + pswd.charAt((secondOccur - 1));
+
+            //System.out.println(line + "," + firstOccur + "," + secondOccur + "," + key + "," + pswd + "," + charFirst + "," + charSecond);
+
+            //if charFirst xor charSecond equals the key, add to validPswd
+            //System.out.println(charFirst.equals(key));
+            //System.out.println(charSecond.equals(key));
+            if((charFirst.equals(key) && !(charSecond.equals(key))) || (!charFirst.equals(key) && (charSecond.equals(key))))
+            {
+                //System.out.println(line);
+                validPswds++;
+            }
+            //System.out.println();
         }
+
+        System.out.println(validPswds);
     }
 }
