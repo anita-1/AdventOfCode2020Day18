bcff9a17bf86735defbc05ce3a2ef3ac9cf536b8
Anita Luo <luo_anita@yahoo.com>
2020-12-24 03:36:04 -0600

    day 2 start

--- src/Day1Main.java
+++ src/Day1Main.java
@@ -24,6 +24,7 @@ public class Day1Main
         //amount of lines
         int numLines = nums.size();
 
+        //number of for loops = amount of elements to add to compare
         for(int index1 = 0; index1 < (numLines - 2); index1++)
         {
             for(int index2 = (index1 + 1); index2 < (numLines - 1); index2++)
@@ -31,6 +32,7 @@ public class Day1Main
                 for(int index3 = (index2 +1); index3 < numLines; index3++)
                 {
                     int result = nums.get(index1) + nums.get(index2) + nums.get(index3);
+                    //if result, then print and break
                     if(result == 2020)
                     {
                         System.out.println(nums.get(index1) * nums.get(index2) * nums.get(index3));
--- src/Day2Main.java
+++ src/Day2Main.java
@@ -1,5 +1,24 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
 
 public class Day2Main
 {
+    public static void main(String[] args) throws Exception
+    {
+        //get file
+        File file = new File("C:\\Users\\unebe\\Documents\\2020 F\\AdventOfCode\\test.txt");
+
+        //read file
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        //add each line to lines
+        String line;
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        while ((line = br.readLine()) != null)
+        {
+            nums.add(Integer.parseInt(line));
+        }
+    }
 }
