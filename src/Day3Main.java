813fb2ccf84bc48053adec3dd35308242fd977be
Anita Luo <luo_anita@yahoo.com>
2020-12-24 23:37:19 -0600

    start day3

--- src/Day3Main.java
+++ src/Day3Main.java
@@ -1,5 +1,23 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 
 public class Day3Main
 {
+    public static void main(String[] args) throws Exception
+    {
+        //get file
+        File file = new File("C:\\Users\\unebe\\Documents\\2020 F\\AdventOfCode\\test.txt");
+
+        //read file
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        //add each line to lines
+        String line;
+        while ((line = br.readLine()) != null)
+        {
+            int cols = line.length();
+            System.out.println(cols);
+        }
+    }
 }
