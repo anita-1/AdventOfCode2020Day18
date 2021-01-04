4940e5c6cbce9a8878dfd8c3d9847c79351a72ae
Anita Luo <luo_anita@yahoo.com>
2021-01-04 17:59:20 -0600

    day4 part1

--- src/Day4Main.java
+++ src/Day4Main.java
@@ -1,5 +1,52 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
 
 public class Day4Main
 {
+    public static void main(String[] args) throws Exception
+    {
+        //get file
+        File file = new File("C:\\Users\\unebe\\Documents\\2020 F\\AdventOfCode\\test.txt");
+
+        //read file
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        //variables info
+        ArrayList<String> inputLines = new ArrayList<String>();
+
+        //add each line to lines
+        String line;
+        String eachPass = "";
+        while ((line = br.readLine()) != null)
+        {
+            eachPass = eachPass + " " + line;
+            //System.out.println("each : " + eachPass);
+            if (line.isBlank())
+            {
+                inputLines.add(eachPass);
+                eachPass = "";
+            }
+        }
+        inputLines.add(eachPass);
+        //System.out.println(inputLines);
+
+        int numValid = 0;
+        for(int index = 0; index < inputLines.size(); index++)
+        {
+            String pass = inputLines.get(index);
+            if( pass.contains("byr") &&
+                    pass.contains("iyr") &&
+                    pass.contains("eyr") &&
+                    pass.contains("hgt") &&
+                    pass.contains("hcl") &&
+                    pass.contains("ecl") &&
+                    pass.contains("pid") )
+            {
+                numValid++;
+            }
+        }
+        System.out.println(numValid);
+    }
 }
