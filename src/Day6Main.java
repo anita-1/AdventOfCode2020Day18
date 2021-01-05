9b2436109af7ffaddc643ff7c4f79591d7ff1f4d
Anita Luo <luo_anita@yahoo.com>
2021-01-05 02:48:00 -0600

    day6 part2 done

--- src/Day6Main.java
+++ src/Day6Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Day6Main
 {
@@ -13,6 +14,39 @@ public class Day6Main
         //read file
         BufferedReader br = new BufferedReader(new FileReader(file));
 
+
+        //PART 2: HOW MANY QUESTIONS ALL PEOPLE ANSWERED 'YES' IN A GROUPS? SUM ALL GROUPS
+        String line = "";
+        ArrayList<String> ppl = new ArrayList<String>();
+        ArrayList<Integer> sums = new ArrayList<Integer>();
+        while ((line = br.readLine()) != null)
+        {
+            //System.out.println(line);
+            if(!line.isBlank())
+            {
+                ppl.add(line);
+            }
+            else if (line.isBlank())
+            {
+                sums.add(allYes(ppl));
+                //end
+                ppl.clear();
+            }
+        }
+        sums.add(allYes(ppl));
+
+        //sum sums
+        int sum = 0;
+        for(int indexSums = 0; indexSums < sums.size(); indexSums++)
+        {
+            sum = sum + sums.get(indexSums);
+        }
+        System.out.println("sum:" + sum);
+
+
+        //
+        //PART 1: HOW MANY QUESTIONS EACH GROUP ANSWERED 'YES'? SUM ALL GROUPS
+        /*
         //list of each group
         ArrayList<String> eachGroupList = new ArrayList<String>();
         String line = "";
@@ -50,5 +84,50 @@ public class Day6Main
             sum = sum + sumEach.get(indexSums);
         }
         System.out.println(sum);
+
+         */
+    }
+
+    static int allYes(ArrayList<String> group)
+    {
+        //System.out.println(group);
+        //find max line and which line that is as person
+        int maxIndivAns = 0;
+        int person = 0;
+        for(int eachPer = 0; eachPer < group.size(); eachPer++)
+        {
+            //maxIndivAns = Math.max(maxIndivAns, group.get(eachPer).length());
+            if(group.get(eachPer).length() > maxIndivAns)
+            {
+                person = eachPer;
+                maxIndivAns = group.get(eachPer).length();
+            }
+        }
+        //System.out.println(maxIndivAns);
+
+
+        int letters = 0;
+        //for each char in person
+        for(int maxIndivIndex = 0; maxIndivIndex < maxIndivAns; maxIndivIndex++)
+        {
+            int manyContains = 0;
+            //for each person in group
+            for(int eachPer = 0; eachPer < group.size(); eachPer++)
+            {
+                String thisPer = group.get(eachPer);
+                //if person contains character
+                if(thisPer.contains("" + group.get(person).charAt(maxIndivIndex)))
+                {
+                    manyContains++;
+                }
+            }
+            //if all persons have the letter
+            if(manyContains == group.size())
+            {
+                letters++;
+            }
+        }
+        //System.out.println(letters);
+        return letters;
     }
 }
